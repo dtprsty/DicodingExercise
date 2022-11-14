6d32588d053bbe26a1ba4d283ddd01f52c598884
dtprasetyo <dprasetyo@waresix.com>
2022-11-14 13:24:21 +0700

    use view binding instead

--- customview-app/src/main/java/com/dtp/customview/Ext.kt
+++ customview-app/src/main/java/com/dtp/customview/Ext.kt
@@ -1,8 +1,13 @@
 package com.dtp.customview
 
 import android.app.Activity
+import android.widget.Toast
 import androidx.annotation.StringRes
 
 fun Activity.string(@StringRes string: Int): String {
     return this.applicationContext.getString(string)
+}
+
+fun Activity.showToast(text: String) {
+    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
 }
--- customview-app/src/main/java/com/dtp/customview/MainActivity.kt
+++ customview-app/src/main/java/com/dtp/customview/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.dtp.customview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.dtp.customview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        initAction()
+    }
+
+    private fun initAction() {
+        binding.apply {
+            customEt.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    //Do nothing
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                    setButtonEnabled()
+                }
+
+                override fun afterTextChanged(p0: Editable?) {
+                    //Do nothing
+                }
+            })
+
+            customBtn.setOnClickListener { showToast(customEt.text.toString()) }
+        }
+    }
+
+    private fun setButtonEnabled() {
+        binding.apply {
+            val result = customEt.text
+            customBtn.isEnabled = !result.toString().isNullOrEmpty()
+        }
     }
 }
